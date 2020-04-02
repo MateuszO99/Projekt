7845e8b619463bfb6c2a55985353d2cf686bde97
Mateusz Or <315352@uwr.edu.pl>
2020-04-02 11:56:32 +0200

    Druga wersja projektu

--- video/urls.py
+++ video/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from .views import (
     VideoListView,
     VideoDetailView,
+    VideoCreateView,
+    VideoDeleteView,
 )
+
+app_name = 'video'
 
 urlpatterns = [
     path('', VideoListView.as_view(), name='main_list'),
-    path('video/<int:pk>/', VideoDetailView.as_view(), name='video_detail'),
+    path('video/<int:pk>/', VideoDetailView.as_view(), name='detail'),
+    path('video/<int:pk>/delete/', VideoDeleteView.as_view(), name='delete'),
+    path('video/create/', VideoCreateView.as_view(), name='create'),
 ]
